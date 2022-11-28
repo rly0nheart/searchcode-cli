ce8507325c686c58468300bbb3e628cdd6a41ca1
Richard Mwewa <74001397+rly0nheart@users.noreply.github.com>
2022-11-28 20:27:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setuptools.setup(
     name="searchcode-cli",
-    version="1.0.0",
+    version="1.1.0",
     author="Richard Mwewa",
     author_email = "rly0nheart@duck.com",
     packages=["searchcode"],
